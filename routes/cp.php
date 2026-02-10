db88838b67c3151517c9f86861588c0a882eeafb
Alt-Charlie <charlie@alt-design.net>
2026-02-10 10:58:10 +0000

    Wrap custom settings in permission

--- routes/cp.php
+++ routes/cp.php
@@ -2,9 +2,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['statamic.cp.authenticated'], 'namespace' => 'AltDesign\AltCookiesAddon\Http\Controllers'], function() {
+    
     // Settings
-    Route::get('/alt-design/alt-cookies/', 'AltCookiesController@index')->name('alt-cookies-addon.index');
-    Route::post('/alt-design/alt-cookies/save', 'AltCookiesController@save')->name('alt-cookies-addon.save');
+    Route::group(['middleware' => ['can:view alt-cookies-addon']], function () {
+        Route::get('/alt-design/alt-cookies/', 'AltCookiesController@index')->name('alt-cookies-addon.index');
+        Route::post('/alt-design/alt-cookies/save', 'AltCookiesController@save')->name('alt-cookies-addon.save');
+    });
     
     // Statamic V6 redirect the Addon settings route
     Route::get('/addons/alt-cookies/settings', fn () => redirect()->to(cp_route('alt-cookies-addon.index')));
