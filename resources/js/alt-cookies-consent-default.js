964915f98149c401ed4f7bb78fc3964c236c6ca6
Alt-Charlie <charlie@alt-design.net>
2026-08-17 11:25:32 +0100

    Removes dev comment

--- resources/js/alt-cookies-consent-default.js
+++ resources/js/alt-cookies-consent-default.js
@@ -4,8 +4,6 @@
     function gtag() { window.dataLayer.push(arguments); }
     window.gtag = window.gtag || gtag;
 
-    // The one place a consent level becomes consent mode signals. The frontend manager reuses
-    // this for the 'update' call when the visitor actually chooses.
     window.altCookiesConsentFor = function (level) {
         var analytics = (level === '2' || level === '4') ? 'granted' : 'denied';
         var advertising = (level === '3' || level === '4') ? 'granted' : 'denied';
